2625744c782d8f65044da20cf87a504599c6f263
Zoe Monet Williams <mwilliamszoe@gmail.com>
2018-08-20 04:50:29 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -16,10 +16,10 @@
 def speak_to_grandma(phrase)
     if phrase == "I LOVE YOU GRANDMA!"
       puts "I LOVE YOU TOO PUMPKIN!"
-    elsif phrase == phrase.downcase
+    elsif phrase != phrase.upcase
   		puts "HUH?! SPEAK UP, SONNY!"
-    elsif phrase == phrase.downcase
-      puts "HUH?! SPEAK UP, SONNY!"
+    elsif phrase == phrase.upcase
+      puts "NO, NOT SINCE 1938!"
     else
   		puts "NO, NOT SINCE 1938!"
     end
